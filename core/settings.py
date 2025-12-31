12c145f0c7c9ed4251bac41d489d5dec0d436c4c
mrgustaveh <mburuantony@outlook.com>
2025-12-31 14:27:23 +0300

    add custom middleware for authorization handling

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -31,6 +32,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # custom middleware
+    'user.middleware.clerkauth.ClerkAuthMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
