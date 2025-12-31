047c96345992da106751537d975ed636c6ba429a
mrgustaveh <mburuantony@outlook.com>
2025-12-31 14:53:00 +0300

    add CHANNEL_LAYERS configuration for Redis

--- core/settings.py
+++ core/settings.py
@@ -55,6 +55,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 DATABASES = {
     'default': {
