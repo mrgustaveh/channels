fd3130d886595ea1c3abb3845422e22b35bac2c7
mrgustaveh <mburuantony@outlook.com>
2026-01-04 15:29:20 +0300

    Add additional URL routes for account listing and retrieval

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,6 @@ from .views import AccountViewset, AccountsListView, AccountRetrieveView
 urlpatterns = [
     path('', AccountViewset.as_view({'get': 'list', 'post': 'create'}), name='accounts-list'),
     path('<str:pk>/', AccountViewset.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update'}), name='accounts-detail'),
+    path('list/', AccountsListView.as_view(), name='accounts-list'),
+    path('retrieve/<str:pk>/', AccountRetrieveView.as_view(), name='accounts-retrieve'),
 ]
